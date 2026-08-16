b4c9070492c2ddeac7ebbbba58c3388bbcc5d0b9
Donovan Moyo <makealivingtoo@gmail.com>
2026-08-16 14:47:14 -0600

    Fix mobile blank screen and remove PWA caching

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,33 +1,15 @@
-const CACHE_NAME = "saskhandy-pwa-v2";
-
-self.addEventListener("install", (event) => {
+self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cacheName) => caches.delete(cacheName))
-      )
-    )
+    Promise.all([
+      caches.keys().then((cacheNames) =>
+        Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)))
+      ),
+      self.registration.unregister(),
+      self.clients.claim(),
+    ])
   );
-
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  const request = event.request;
-
-  if (request.method !== "GET") {
-    return;
-  }
-
-  const url = new URL(request.url);
-
-  if (url.pathname.startsWith("/api/")) {
-    return;
-  }
-
-  event.respondWith(fetch(request));
 });
